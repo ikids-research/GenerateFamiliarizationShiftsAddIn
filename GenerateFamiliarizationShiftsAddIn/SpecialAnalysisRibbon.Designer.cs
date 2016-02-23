4885842a3537340b0d74c4a04c677fd72e4bd116
kevroy314 <kevin.horecka@gmail.com>
2016-02-22 18:28:41 -0600

    Decompressed and Added Cert

---
+++ GenerateFamiliarizationShiftsAddIn/SpecialAnalysisRibbon.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace GenerateFamiliarizationShiftsAddIn
+{
+    partial class SpecialAnalysisRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public SpecialAnalysisRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SpecialAnalysisRibbon));
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.runButton = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.runButton);
+            this.group1.Label = "Run";
+            this.group1.Name = "group1";
+            // 
+            // runButton
+            // 
+            this.runButton.Image = ((System.Drawing.Image)(resources.GetObject("runButton.Image")));
+            this.runButton.Label = "Generate Familiarization Shift Summary";
+            this.runButton.Name = "runButton";
+            this.runButton.ShowImage = true;
+            this.runButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.runButton_Click);
+            // 
+            // SpecialAnalysisRibbon
+            // 
+            this.Name = "SpecialAnalysisRibbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.SpecialAnalysisRibbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton runButton;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal SpecialAnalysisRibbon SpecialAnalysisRibbon
+        {
+            get { return this.GetRibbon<SpecialAnalysisRibbon>(); }
+        }
+    }
+}
---
+++ GenerateFamiliarizationShiftsAddIn/SpecialAnalysisRibbon.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Interop.Excel;
+using System.Windows.Forms;
+
+namespace GenerateFamiliarizationShiftsAddIn
+{
+    public partial class SpecialAnalysisRibbon
+    {
+        private void SpecialAnalysisRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void runButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            Window window = e.Control.Context;
+            List<string> names = new List<string>();
+            List<int> vals = new List<int>();
+            List<int> output = new List<int>();
+
+            try
+            {
+                foreach (Worksheet sheet in window.Application.ActiveWorkbook.Worksheets)
+                {
+                    names.Add(sheet.Name);
+
+                    Range r0 = sheet.get_Range("F4");
+                    Range r1 = sheet.get_Range("G3");
+
+                    Range r2 = sheet.get_Range("I7");
+                    Range r3 = sheet.get_Range("J6");
+
+                    Range r4 = sheet.get_Range("L10");
+                    Range r5 = sheet.get_Range("M9");
+
+                    Range r6 = sheet.get_Range("O13");
+                    Range r7 = sheet.get_Range("P12");
+
+                    Range r8 = sheet.get_Range("R16");
+                    Range r9 = sheet.get_Range("S15");
+
+                    Range[] cells = { r0, r1, r2, r3, r4, r5, r6, r7, r8, r9 };
+                    int o = 1;
+                    for (int i = 0; i < cells.Length; i += 2)
+                    {
+                        int sum = 0;
+                        if (cells[i].Value2 == null || cells[i + 1].Value2 == null)
+                            o = 0;
+                        if (!(cells[i].Value2 == null))
+                            sum += (int)cells[i].Value2;
+                        if (!(cells[i + 1].Value2 == null))
+                            sum += (int)cells[i + 1].Value2;
+
+                        vals.Add(sum);
+                    }
+                    output.Add(o);
+                }
+            }
+            catch (Exception) { MessageBox.Show("Error parsing input files sheets."); return; }
+
+            try {
+                Worksheet s = window.Application.ActiveWorkbook.Worksheets.Add();
+
+                s.Name = "FamShiftOutput";
+
+                generateHeader(s);
+
+                for (int i = 0; i < names.Count; i++)
+                {
+                    int i2 = i + 2;
+                    s.get_Range("A" + i2).Value = names[i];
+                    s.get_Range("B" + i2).Value = output[i];
+
+                    int root = i * 5;
+
+                    s.get_Range("C" + i2).Value = vals[root + 0];
+                    s.get_Range("D" + i2).Value = vals[root + 1];
+                    s.get_Range("E" + i2).Value = vals[root + 2];
+                    s.get_Range("F" + i2).Value = vals[root + 3];
+                    s.get_Range("G" + i2).Value = vals[root + 4];
+
+                    s.get_Range("C" + i2).Interior.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.FromArgb(189, 215, 238));
+                    s.get_Range("D" + i2).Interior.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.FromArgb(255, 204, 204));
+                    s.get_Range("E" + i2).Interior.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.FromArgb(198, 244, 180));
+                    s.get_Range("F" + i2).Interior.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.FromArgb(204, 204, 255));
+                    s.get_Range("G" + i2).Interior.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.FromArgb(248, 203, 173));
+                }
+
+                s.Columns.AutoFit();
+            }
+            catch (Exception) { MessageBox.Show("Error generating output sheet."); return; }
+
+            MessageBox.Show("Success");
+        }
+
+        private void generateHeader(Worksheet s)
+        {
+            s.get_Range("A1").Value = "ID";
+            s.get_Range("B1").Value = "Correct_Output";
+            s.get_Range("C1").Value = "Fam1_Shifts";
+            s.get_Range("D1").Value = "Fam2_Shifts";
+            s.get_Range("E1").Value = "Fam3_Shifts";
+            s.get_Range("F1").Value = "Fam4_Shifts";
+            s.get_Range("G1").Value = "Fam5_Shifts";
+        }
+    }
+}
